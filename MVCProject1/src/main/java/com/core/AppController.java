8f46e026966cfa50ad68625f68f5715fd01c2b4c
Omkar Mandavkar <73053306+OmkarMandavkar@users.noreply.github.com>
2024-12-15 20:13:41 +0530

    Update AppController.java

--- MVCProject1/src/main/java/com/core/AppController.java
+++ MVCProject1/src/main/java/com/core/AppController.java
@@ -3,15 +3,17 @@ package com.core;
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class AppController {
 
 	@RequestMapping(value = "/message", method = RequestMethod.GET) // will get triggered
 	public String getMessage() { // only for get requests
-		return "test.jsp";
+		return "display1.jsp";
 	}
 
 	@RequestMapping(value = "/message", method = RequestMethod.POST) // will get triggered
@@ -19,17 +21,49 @@ public class AppController {
 		return "demo.jsp";
 	}
 
+	// Reading HTML form data in API and print it on the console
 	@RequestMapping(value = "/read", method = RequestMethod.POST)
-	public String read(HttpServletRequest request) {
+	public ModelAndView read(HttpServletRequest request) {
 
 		String username = request.getParameter("userName");
 		String useremail = request.getParameter("userEmail");
 		String address = request.getParameter("address");
 
-		System.out.println("UserName" + username);
-		System.out.println("UserEmail" + useremail);
-		System.out.println("Address" + address);
+		System.out.println("UserName : " + username);
+		System.out.println("UserEmail : " + useremail);
+		System.out.println("Address : " + address);
 
-		return "test.jsp";
+		ModelAndView mv = new ModelAndView("display1.jsp");
+		mv.addObject("UName", username);
+		mv.addObject("UEmail", useremail);
+		mv.addObject("UAddress", address);
+
+		return mv;
+	}
+
+	// Passing the data from controller to view and ask view to print it using ModelAndView class object
+	@RequestMapping("/write")
+	public ModelAndView write() {
+
+		ModelAndView mv = new ModelAndView("display2.jsp");
+
+//		mv.setViewName("display2.jsp");
+		mv.addObject("UName", "Zeus");
+		mv.addObject("UEmail", "zeus@gmail.com");
+		mv.addObject("UAddress", "Powai");
+
+		return mv;
+	}
+
+	
+	// Passing the data between controller and view using model interface
+	@RequestMapping("/msg")
+	public String message(Model model) {
+		
+		model.addAttribute("brand", "ABC");
+		model.addAttribute("name", "XYZ");
+		model.addAttribute("age", 20);
+		
+		return "display3.jsp";
 	}
 }
